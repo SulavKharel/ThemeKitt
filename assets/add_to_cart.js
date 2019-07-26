85d5968e254cd8af9035545cb57299ac052decf7
sulav kharel <kharel.sulav2016@gmail.com>
2019-07-27 01:00:11 +0900

    added shopify jquery library

--- assets/add_to_cart.js
+++ assets/add_to_cart.js
@@ -1,7 +1,9 @@
-function() {
+const AddToCartAjax = () {
   const form = document.querySelector('#AddToCartForm');
   form.onsubmit = (event) => {
     event.preventDefault();
     Shopify.addItemFromForm("AddToCartForm");
   };
 }
+
+AddToCartAjax();
